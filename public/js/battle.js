312165a7d5a0d9ea4b2c2ba944df045f7614af60
Emily Rendleman <emilyrendleman@gmail.com>
2021-07-14 22:14:00 -0500

    teampage button added to battle page

--- public/js/battle.js
+++ public/js/battle.js
@@ -136,6 +136,20 @@ const createEnemyCard = () => {
 
 createEnemyCard();
 
+const backButtonHandler = () => {
+  fetch('/api/users/', {
+    method: 'GET',
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.id);
+      document.location.replace(`/teampage/${data.id}`);
+    });
+};
+
 document
   .querySelector('#commence-battle')
   .addEventListener('click', getUserHero);
+document
+  .querySelector('#back-btn')
+  .addEventListener('click', backButtonHandler);
